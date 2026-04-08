dc8ef283cb01069c0edae9604c64e9b8a02e215e
Christian Lopes <chrtannus@gmail.com>
2026-04-08 14:59:22 -0300

    Moves the `scroll-behavior` property to the css file.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={clsx('h-full bg-gray-50 antialiased scroll-smooth' /*, inter.variable */)}
+      className={clsx('h-full bg-gray-50 antialiased' /*, inter.variable */)}
     >
       <body className="flex h-full flex-col">
         <div className="flex min-h-full flex-col">{children}</div>
