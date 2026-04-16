f45c43a8b8dd315c6b25b9e109b1a76fba005506
Christian Lopes <chrtannus@gmail.com>
2026-04-16 16:21:10 -0300

    Minor code refactoring.

--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -15,9 +15,9 @@ const Citation = ({ href, className, children }) => {
 
   return (
     <code className={className}>
-      <a href={href} target="_blank" rel="noreferrer" className="no-underline hover:text-complement-500">
+      <LinkOut href={href} className="no-underline">
         {children}
-      </a>
+      </LinkOut>
     </code>
   )
 }
